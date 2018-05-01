f07e05e1a67484c56f6907e0cfeef8e20cdc6506
Stefan Stappen <stefan.stappen@xion.at>
2018-05-01 22:54:58 +0200

    merge lines finished, edges share line too, integrated in the recognition process, works quite good for many but not the mouse though

---
+++ PageDetection Code/mergeLines.m
@@ -0,0 +1,70 @@
+function  newLines  = mergeLines( lines, margin )
+%MERGELINES merge lines within pixel margin. 
+% All lines sharing a common endpoint within a circular marign are merged
+% returns the merged and reduced set of lines
+numLines = size(lines,2);
+
+for hLines=1:size(lines,2)
+    found = 1;
+    
+    foundLines = [lines(:, hLines)];
+    prevEqual = false(1,size(lines, 2));
+    prevEqual(hLines) = 1;
+    
+    
+    while found <= size(foundLines,2)
+       x1_1Equal = repmat(foundLines(1, found) - margin, 1, numLines)  <= lines(1, :) ...
+                & repmat(foundLines(1, found) + margin, 1, numLines)  >= lines(1, :);
+       x1_2Equal = repmat(foundLines(1, found) - margin, 1, numLines)  <= lines(2, :) ...
+                & repmat(foundLines(1, found) + margin, 1, numLines)  >= lines(2, :);
+       x2_1Equal = repmat(foundLines(2, found) - margin, 1, numLines)  <= lines(1, :) ...
+                & repmat(foundLines(2, found) + margin, 1, numLines)  >= lines(1, :);
+       x2_2Equal = repmat(foundLines(2, found) - margin, 1, numLines)  <= lines(2, :) ...
+                & repmat(foundLines(2, found) + margin, 1, numLines)  >= lines(2, :);
+            
+       y1_1Equal = repmat(foundLines(3, found) - margin, 1, numLines)  <= lines(3, :) ...
+                & repmat(foundLines(3, found) + margin, 1, numLines)  >= lines(3, :);
+       y1_2Equal = repmat(foundLines(3, found) - margin, 1, numLines)  <= lines(4, :) ...
+                & repmat(foundLines(3, found) + margin, 1, numLines)  >= lines(4, :);
+       y2_1Equal = repmat(foundLines(4, found) - margin, 1, numLines)  <= lines(3, :) ...
+                & repmat(foundLines(4, found) + margin, 1, numLines)  >= lines(3, :);
+       y2_2Equal = repmat(foundLines(4, found) - margin, 1, numLines)  <= lines(4, :) ...
+                & repmat(foundLines(4, found) + margin, 1, numLines)  >= lines(4, :);
+            
+       equal = x1_1Equal & y1_1Equal | x1_2Equal & y1_2Equal ...
+           | x2_1Equal & y2_1Equal | x2_2Equal & y2_2Equal;
+
+        found = found + 1;
+        
+        foundLines = [foundLines, lines(:, equal & ~prevEqual)];
+        
+        prevEqual = prevEqual | equal;
+    end
+    
+    % sets all found to zero so they are not found again
+    lines(:, prevEqual) = zeros(size(lines,1), sum(prevEqual));
+    
+    [minX1, minX1I] = min(foundLines(1,:));
+    [minX2, minX2I] = min(foundLines(2,:));
+    
+    [minX, minXI] = min([minX1, minX2]);
+    minXI_R = (1-(minXI-1)) * minX1I + (minXI-1) * minX2I;
+    minY = min(foundLines(minXI + 2,minXI_R));
+    
+    [maxX1, maxX1I] = max(foundLines(1,:));
+    [maxX2, maxX2I] = max(foundLines(2,:));
+    
+    [maxX, maxXI] = max([maxX1, maxX2]);
+    maxXI_R = (1-(maxXI-1)) * maxX1I + (maxXI-1) * maxX2I;
+    maxY = max(foundLines(maxXI + 2,maxXI_R));
+    
+    lines(:, hLines) =  [minX maxX minY maxY max(foundLines(5,:))];
+    %[min(min(foundLines(1,:)),min(foundLines(2,:))) max(max(foundLines(1,:)),max(foundLines(2,:)))...
+    %     min(min(foundLines(3,:)),min(foundLines(4,:))) max(max(foundLines(3,:)),max(foundLines(4,:))) max(foundLines(5,:))];
+    
+end
+
+newLines = lines(:, lines(5,:) ~= 0);
+
+end
+
--- PageDetection Code/pageDetection.m
+++ PageDetection Code/pageDetection.m
@@ -1,7 +1,17 @@
 %% Load videframes
 ImagePath='./images/myImage.jpg';
-v = VideoReader('./video/magazine003.avi');
-vImage=read(v,50);
+v = VideoReader('./video/datasheet001.avi');
+vImage=read(v,10);
+
+%% parameters
+
+margin = 10
+quadMargin = 50
+maxLength = 3000
+maxAreaPercentage = 0.20
+minLengthPercentage = 0.10
+
+%%
 
 %vImage = rgb2gray(vImage);
 %level = graythresh(vImage);
@@ -15,7 +25,15 @@ image = imread(ImagePath);
 %% get the start_points and end_points of each straight line use LSD.
 lines = lsd(ImagePath);
 
-
+img_width=size(image,2);
+img_height=size(image,1);
+length=(lines(1,:)-lines(2,:)).^2+(lines(3,:)-lines(4,:)).^2;
+indices=find(length> ( img_width + img_height)/2 * minLengthPercentage & ...                                              %length removal
+    lines(1,:)<img_width*0.8 & lines(1,:)>img_width*0.2 & ...      %surrounding x1 removal
+    lines(2,:)<img_width*0.8 & lines(2,:)>img_width*0.2 & ...      %surrounding x2 removal
+    lines(3,:)<img_height*0.9 & lines(3,:)>img_height*0.1 & ...    %surrounding y1 removal
+    lines(4,:)<img_height*0.9 & lines(4,:)>img_height*0.1);        %surrounding y2 removal
+lines=lines(:,indices);
 
 
 % labImage = rgb2lab(image);
@@ -23,92 +41,48 @@ lines = lsd(ImagePath);
 % hisImage=histeq(lImage);
 % grayImage=rgb2gray(image);
 
-
-%% discard lines
-
-img_width=size(image,2);
-img_height=size(image,1);
-length=(lines(1,:)-lines(2,:)).^2+(lines(3,:)-lines(4,:)).^2;
-indices=find(length>0 & ...                                              %length removal
-    lines(1,:)<img_width*0.8 & lines(1,:)>img_width*0.2 & ...      %surrounding x1 removal
-    lines(2,:)<img_width*0.8 & lines(2,:)>img_width*0.2 & ...      %surrounding x2 removal
-    lines(3,:)<img_height*0.9 & lines(3,:)>img_height*0.1 & ...    %surrounding y1 removal
-    lines(4,:)<img_height*0.9 & lines(4,:)>img_height*0.1);        %surrounding y2 removal
-lines=lines(:,indices);
+% group lines
 indicesVertical=find(abs(lines(1,:)-lines(2,:))./abs(lines(3,:)-lines(4,:))<1);
 indicesHorizontal=find(abs(lines(1,:)-lines(2,:))./abs(lines(3,:)-lines(4,:))>1);
 linesVertical=lines(:,indicesVertical);
 linesHorizontal=lines(:,indicesHorizontal);
 
-%% join lines
-%liHorX = linesHorizontal(1:2, :);
-%liHorY = linesHorizontal(3:4, :);
-%liHorX(3,:) = NaN;
-%liHorY(3,:) = NaN;
+%% mergelines
 
-%[x, y] = polymerge(liHorX(:), liHorY(:), 10);
+linesVertical = mergeLines(linesVertical, margin);
+linesHorizontal = mergeLines(linesHorizontal, margin);
 
-margin = 10;
+%% discard lines
 
-numLines = size(linesHorizontal,2);
+length=(linesVertical(1,:)-linesVertical(2,:)).^2+(linesVertical(3,:)-linesVertical(4,:)).^2;
+indices=find(length>maxLength);        %surrounding y2 removal
+linesVertical=linesVertical(:,indices);
 
-for hLines=1:size(linesHorizontal,2)
-    found = 1;
-    
-    foundLines = [linesHorizontal(:, hLines)];
-    prevEqual = false(1,65);
-    prevEqual(hLines) = 1;
-    
-    
-    while found <= size(foundLines,2)
-       x1_1Equal = repmat(foundLines(1, found) - margin, 1, numLines)  <= linesHorizontal(1, :) ...
-                & repmat(foundLines(1, found) + margin, 1, numLines)  >= linesHorizontal(1, :);
-       x1_2Equal = repmat(foundLines(1, found) - margin, 1, numLines)  <= linesHorizontal(2, :) ...
-                & repmat(foundLines(1, found) + margin, 1, numLines)  >= linesHorizontal(2, :);
-       x2_1Equal = repmat(foundLines(2, found) - margin, 1, numLines)  <= linesHorizontal(1, :) ...
-                & repmat(foundLines(2, found) + margin, 1, numLines)  >= linesHorizontal(1, :);
-       x2_2Equal = repmat(foundLines(2, found) - margin, 1, numLines)  <= linesHorizontal(2, :) ...
-                & repmat(foundLines(2, found) + margin, 1, numLines)  >= linesHorizontal(2, :);
-            
-       y1_1Equal = repmat(foundLines(3, found) - margin, 1, numLines)  <= linesHorizontal(3, :) ...
-                & repmat(foundLines(3, found) + margin, 1, numLines)  >= linesHorizontal(3, :);
-       y1_2Equal = repmat(foundLines(3, found) - margin, 1, numLines)  <= linesHorizontal(4, :) ...
-                & repmat(foundLines(3, found) + margin, 1, numLines)  >= linesHorizontal(4, :);
-       y2_1Equal = repmat(foundLines(4, found) - margin, 1, numLines)  <= linesHorizontal(3, :) ...
-                & repmat(foundLines(4, found) + margin, 1, numLines)  >= linesHorizontal(3, :);
-       y2_2Equal = repmat(foundLines(4, found) - margin, 1, numLines)  <= linesHorizontal(4, :) ...
-                & repmat(foundLines(4, found) + margin, 1, numLines)  >= linesHorizontal(4, :);
-            
-       equal = x1_1Equal & y1_1Equal | x1_2Equal & y1_2Equal ...
-           | x2_1Equal & y2_1Equal | x2_2Equal & y2_2Equal;
-
-        found = found + 1;
-        
-        foundLines = [foundLines, linesHorizontal(:, equal & ~prevEqual)];
-        
-        prevEqual = prevEqual | equal;
-    end
-    
-    % sets all found to zero so they are not found again
-    linesHorizontal(:, prevEqual) = zeros(size(linesHorizontal,1), sum(prevEqual));
-    linesHorizontal(:, hLines) =  [min(min(foundLines(1,:)),min(foundLines(2,:))) max(max(foundLines(1,:)),max(foundLines(2,:)))...
-         min(min(foundLines(3,:)),min(foundLines(4,:))) max(max(foundLines(3,:)),max(foundLines(4,:))), max(foundLines(5,:))];
-    
-end
-
-linesHorizontal = linesHorizontal(:, linesHorizontal(5,:) ~= 0);
+length=(linesHorizontal(1,:)-linesHorizontal(2,:)).^2+(linesHorizontal(3,:)-linesHorizontal(4,:)).^2;
+indices=find(length>maxLength);        %surrounding y2 removal
+linesHorizontal=linesHorizontal(:,indices);
 
 
 %% compute bounding boxes
 
-%boundingBoxes = [];
+boundingBoxes = [];
 i = 0;
 for hLines1=1:size(linesHorizontal,2)
     for hLines2=hLines1+1:size(linesHorizontal,2)
         for vLines1=1:size(linesVertical,2)
             for vLines2=vLines1+1:size(linesVertical,2)
+                hLine1 = linesHorizontal(1:4,hLines1);
+                hLine2 = linesHorizontal(1:4,hLines2);
+                vLine1 = linesVertical(1:4,vLines1);
+                vLine2 = linesVertical(1:4,vLines2);
+                 
+                if (edgesSharePoint(hLine1, vLine1, quadMargin) & ... 
+                    edgesSharePoint(hLine1, vLine2, quadMargin) & ...
+                    edgesSharePoint(hLine2, vLine1, quadMargin) & ... 
+                    edgesSharePoint(hLine2, vLine2, quadMargin) )
                 i = i+1;
-                %boundingBoxes(i, :, :) = calcBoundingBox(linesHorizontal(1:4,hLines1),linesHorizontal(1:4,hLines2),linesVertical(1:4,vLines1),linesVertical(1:4,vLines2));
+                boundingBoxes(i, :, :) = calcBoundingBox(hLine1,hLine2,vLine1,linesVertical(1:4,vLines2));
+                end
             end
         end
     end
@@ -117,7 +91,7 @@ end
 % choose best quad
 maxArea = 0;
 bestBoundingBox = [];
-
+areaLimit = img_width * img_height * maxAreaPercentage;
 
 for i = 1:size(boundingBoxes,1)
     % reorder corner points
@@ -131,21 +105,22 @@ for i = 1:size(boundingBoxes,1)
     lengthHorz1=sqrt(((boundingBox(1,1)-boundingBox(1,2)).^2+(boundingBox(2,1)-boundingBox(2,2)).^2));
     lengthHorz2=sqrt(((boundingBox(1,3)-boundingBox(1,4)).^2+(boundingBox(2,3)-boundingBox(2,4)).^2));
     if abs(lengthHorz1-lengthHorz2)>0.2*lengthHorz1
-        continue;
+        %continue;
     end
     lengthVert1=sqrt(((boundingBox(1,1)-boundingBox(1,4)).^2+(boundingBox(2,1)-boundingBox(2,4)).^2));
     lengthVert2=sqrt(((boundingBox(1,2)-boundingBox(1,3)).^2+(boundingBox(2,2)-boundingBox(2,3)).^2));
     if abs(lengthVert1-lengthVert2)>0.2*lengthVert1
-        continue;
+        %continue;
     end
     
     %calculate aspectratio
     %aspectRatio A4paper 1/sqrt(2)=0.707 querformat 1.414
-    aspectRatio=lengthHorz1/lengthVert1;
+    aspectRatio=(max(lengthHorz1,lengthHorz2)/min(lengthVert1,lengthVert2) + ...
+        min(lengthHorz1,lengthHorz2)/max(lengthVert1,lengthVert2))/2;
     a4AR = 1/sqrt(2);
     
-    if (aspectRatio < a4AR-0.05 || aspectRatio > a4AR+0.05) && ...
-        (aspectRatio < 1/a4AR-0.05 || aspectRatio > 1/a4AR+0.05)
+    if (aspectRatio < a4AR-0.25 || aspectRatio > a4AR+0.25) && ...
+        (aspectRatio < 1/a4AR-0.25 || aspectRatio > 1/a4AR+0.25)
         continue;
     end
     
@@ -160,7 +135,7 @@ for i = 1:size(boundingBoxes,1)
     end
     
     
-    if area > maxArea && area < 250000
+    if area > maxArea && area < areaLimit
         maxArea = area;
         bestBoundingBox = boundingBox;
     end
@@ -175,7 +150,8 @@ bestBoundingBox
  lengthVert1=sqrt(((bestBoundingBox(1,1)-bestBoundingBox(1,4)).^2+(bestBoundingBox(2,1)-bestBoundingBox(2,4)).^2))
  lengthVert2=sqrt(((bestBoundingBox(1,2)-bestBoundingBox(1,3)).^2+(bestBoundingBox(2,2)-bestBoundingBox(2,3)).^2))
  area
- aspectRatio=lengthHorz1/lengthVert1
+ aspectRatio=(max(lengthHorz1,lengthHorz2)/min(lengthVert1,lengthVert2) + ...
+        min(lengthHorz1,lengthHorz2)/max(lengthVert1,lengthVert2))/2
 
 vectors = circshift(bestBoundingBox, -1, 2) - bestBoundingBox;
 vectors = vectors./sqrt(vectors(1,:).^2+vectors(2,:).^2);
@@ -193,6 +169,9 @@ end
 for i = 1:size(linesHorizontal, 2)
     plot(linesHorizontal(1:2, i), linesHorizontal(3:4, i), 'LineWidth', linesHorizontal(5, i) / 2, 'Color', [1, 0, 0]);
 end
+for i = 1:size(linesVertical, 2)
+    plot(linesVertical(1:2, i), linesVertical(3:4, i), 'LineWidth', linesVertical(5, i) / 2, 'Color', [1, 0, 0]);
+end
 
 
-    plot(bestBoundingBox(1,:), bestBoundingBox(2,:), 'LineWidth', 3, 'Color', [0, 0, 1]);
+plot(bestBoundingBox(1,:), bestBoundingBox(2,:), 'LineWidth', 3, 'Color', [0, 0, 1]);
